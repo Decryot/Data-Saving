ea4430fa5dd7bcfec7cee2a3255b96d8ed6123e3
Dmitri <56200546+Novurix@users.noreply.github.com>
2020-06-29 11:56:44 -0400

    C# Data Saving

---
+++ CSharp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace CsharpDataSaving
+{
+    class FileCreator {
+        string pathToFolder = Directory.GetCurrentDirectory();
+        string folderName = "DATA";
+
+        public FileCreator() {
+            if (!Directory.Exists(pathToFolder + "/" + folderName)) {
+                Directory.CreateDirectory(pathToFolder + "/" + folderName);
+            }
+
+            if (!Directory.Exists(pathToFolder + "/" + folderName + "/data.txt")) {
+
+                try { 
+                    TextWriter writer = new StreamWriter(pathToFolder+"/"+folderName+"/data.txt", true);
+
+                    writer.Write("10");
+                    writer.Flush();
+                    writer.Close();
+                }
+                catch (Exception e) { Console.WriteLine(e); }
+            }
+        }
+
+        public int getData() {
+
+            int loadedData = 0;
+            TextReader reader = new StreamReader(pathToFolder+"/"+folderName+"/data.txt", true);
+
+            string line = reader.ReadLine();
+            loadedData = Int32.Parse(line);
+            return loadedData;
+        }
+    }
+
+    class Program {
+
+        static FileCreator fileCreator;
+        static int data = 0;
+
+        public static void Main(string[] args) {
+
+            fileCreator = new FileCreator();
+            data = fileCreator.getData();
+            Console.Write(data);
+        }
+    }
+}
