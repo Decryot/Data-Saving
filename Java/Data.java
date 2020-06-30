906a06412f897c1bb1a5622df15ec53b81714176
Dmitri <56200546+Novurix@users.noreply.github.com>
2020-06-30 12:33:43 -0400

    added save data method

--- Java/Data.java
+++ Java/Data.java
@@ -1,17 +1,31 @@
-package Java;
+
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Data {
 
     static int loadedData;
     static Scanner fileScanner;
+    static PrintWriter fileWriter;
 
     public static void main(String[] args) throws FileNotFoundException {
+        saveData(100, "Data.txt");
         loadedData = loadData("Data.txt");
+        
         System.out.println(loadedData);
+    }
+
+    static void saveData(int data, String fileName) throws FileNotFoundException {
+        File dataFile = new File(fileName);
+        if (dataFile.exists()) {
+            fileWriter = new PrintWriter(dataFile);
+            fileWriter.println(data);
+            fileWriter.flush();
+            fileWriter.close();
+        }
     }
 
     static int loadData(String fileName) throws FileNotFoundException {
